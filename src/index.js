7963c749a0a3f4e9397deb115942ae41161dff6b
Vision-X <michael.s.marlow@gmail.com>
2018-08-06 17:36:58 -0600

    add component for adding links to db, router used for custom route to component, post req built out

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import AddLink from './AddLink';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={ App } />
+      <Route path="/add" component={ AddLink } />
+    </Fragment>
+  </Router>,
+document.getElementById('root'));
 registerServiceWorker();
